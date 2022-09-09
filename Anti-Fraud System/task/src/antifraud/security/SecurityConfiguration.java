f4ab53539c3f81730e99c8fc966a05c149c5d504
Grzegorz Piecyk <grzegorzpiecyk@gmail.com>
2022-09-09 22:53:17 +0200

    Stage 2/6: Added Spring Security

---
+++ Anti-Fraud System/task/src/antifraud/security/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package antifraud.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final AuthenticationEntryPoint restAuthenticationEntryPoint = new RestAuthenticationEntryPoint();
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .authenticationEntryPoint(restAuthenticationEntryPoint) // Handles auth error
+                .and()
+                .csrf().disable().headers().frameOptions().disable() // for Postman, the H2 console
+                .and()
+                .authorizeRequests() // manage access
+                .antMatchers(HttpMethod.POST, "/api/auth/user").permitAll()
+                .antMatchers("/actuator/shutdown").permitAll() // needs to run test
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // no session
+    }
+}
